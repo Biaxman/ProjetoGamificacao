5d7077a895ead76297f6df4c7e5fc063d84bbdc4
Longarini <vlongarini@outlook.com>
2022-09-18 14:21:52 -0300

    Commit de  Metodos de Manipulação de Tarefas.

--- api/controller/tarefas.js
+++ api/controller/tarefas.js
@@ -1,8 +1,59 @@
 module.exports = app => {
+    const fs = require('fs');
+
     const tarefasDB = app.data.tarefas;
     const controller = {};
 
-    controller.listTarefas = (req, res) => res.status(200).json(tarefasDB);
+    controller.listTarefas = (req, res) => {
+        var dataRead;
+        fs.readFile('./api/data/tarefas.json','utf-8',(err,data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            dataRead = JSON.parse(data);
+            res.status(200).json(dataRead.tarefas);
+          });
+    }
+
+    controller.listTarefaId = (req, res) => {
+        var dataRead;
+        
+        const {
+            tarefaId,
+          } = req.params;
+
+        fs.readFile('./api/data/tarefas.json','utf-8',(err,data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            
+            dataRead = JSON.parse(data);
+            var tarefa = dataRead.tarefas.find(tarefa => tarefa.id === tarefaId);
+            res.status(200).json(tarefa);
+          });
+    }
+
+    controller.removeTarefaId = (req, res) => {
+        var dataRead;
+        
+        const {
+            tarefaId,
+          } = req.params;
+
+        fs.readFile('./api/data/tarefas.json','utf-8',(err,data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            
+            dataRead = JSON.parse(data);
+            var index = dataRead.tarefas.findIndex(tarefa => tarefa.id >= tarefaId);
+            dataRead.tarefas.splice(index,1);
+            fs.writeFile('./api/data/tarefas.json',JSON.stringify(dataRead) ,'utf-8',()=>{console.log('OK')});
+          });
+      };
 
     return controller;
 }
--- api/routes/tarefas.js
+++ api/routes/tarefas.js
@@ -3,4 +3,8 @@ module.exports = app => {
 
     app.route('/api/v1/tarefas')
     .get(controller.listTarefas);
+    
+    app.route('/api/v1/tarefaId/:tarefaId')
+    .get(controller.listTarefaId)
+    .delete(controller.removeTarefaId);
 }
