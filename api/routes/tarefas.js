97f3d5601eaf2b26a455e2ea7ce53a5babbceccc
Vitor Longarini <vlongarini@outlook.com>
2022-09-18 18:01:51 -0300

    Finalização dos Metodos das Tarefas.

--- api/routes/tarefas.js
+++ api/routes/tarefas.js
@@ -2,7 +2,9 @@ module.exports = app => {
     const controller = app.controller.tarefas;
 
     app.route('/api/v1/tarefas')
-    .get(controller.listTarefas);
+    .get(controller.listTarefas)
+    .post(controller.createTarefa)
+    .put(controller.updateTarefa);
     
     app.route('/api/v1/tarefaId/:tarefaId')
     .get(controller.listTarefaId)
